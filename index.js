3fb819c9a9f38eada9bec4fcbce13babb573542a
Shaun Ripley <shaun.ripley@gmail.com>
2022-03-10 17:12:51 +1300

    Second session

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-const books = [
+const booksAction = [
     {
-        name: 'Book 1',
+        name: 'Action Book 1',
         author: 'Some Author 1',
     },
     {
-        name: 'Book 2',
+        name: 'Action Book 2',
         author: 'Some Author 2',
     },
 ];
 
-let htmlBody = '';
-for ( let i = 0; i < books.length; i++ ) {
-    const book = books[i];
-    const bookArray = Object.values(book);
+const booksAdventure = [
+    {
+        name: 'Adventure Book 1',
+        author: 'Some Author 1',
+    },
+    {
+        name: 'Adventure  Book 2',
+        author: 'Some Author 2',
+    },
+];
 
-    for ( let j = 0; j < bookArray.length; j++ ) {
-        htmlBody += `<tr><td>${bookArray[j]}</td></tr>`;
+
+function renderTable(bookParam) {
+    if (bookParam.length === 0) {
+        console.error('Books has an unexpected size of 0');
+        return 'An error occurred, please contact support';
     }
+
+    // Render the html table body tag
+    let htmlBody = '';
+    for (let i = 0; i < bookParam.length; i++) {
+        const book = bookParam[i];
+        const bookArray = Object.values(book);
+
+        htmlBody += '<tr>';
+        for (let j = 0; j < bookArray.length; j++) {
+            htmlBody += `<td>${bookArray[j]}</td>`;
+        }
+        htmlBody += '</tr>';
+    }
+
+    // Render the html table head tag
+    let htmlThead = '';
+
+    const book = bookParam[0];
+    const bookArray = Object.keys(book);
+
+    htmlThead += '<tr>';
+    for (let j = 0; j < bookArray.length; j++) {
+        htmlThead += `<th>${bookArray[j]}</th>`;
+    }
+    htmlThead += '</tr>';
+
+    return `<table class="table-book"><thead>${htmlThead}</thead><tbody>${htmlBody}</tbody></table>`;
 }
-document.body.innerHTML = `<table>${htmlBody}</table>`;
+
+document.body.innerHTML = `<div class="d-inline mr-2">${renderTable(booksAction)}</div><div class="d-inline">${renderTable(booksAdventure)}</div>`;
