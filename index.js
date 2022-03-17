296d36c4aec6b3a3e03e82ddb644b085484cf942
Shaun Ripley <shaun.ripley@gmail.com>
2022-03-17 17:20:50 +1300

    Session 3

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const booksAction = [
     {
         name: 'Action Book 1',
         author: 'Some Author 1',
+        description: '123 long text',
+        image: 'action-book-1.jpg',
     },
     {
         name: 'Action Book 2',
-        author: 'Some Author 2',
     },
 ];
 
@@ -13,13 +14,49 @@ const booksAdventure = [
     {
         name: 'Adventure Book 1',
         author: 'Some Author 1',
+        image: 'default-book.png',
     },
     {
         name: 'Adventure  Book 2',
         author: 'Some Author 2',
+        image: 'default-book.png',
     },
 ];
 
+const renderLabelValue = (label, value) => `
+    <div>
+        <span class="label">${label}</span>
+        <span class="label-text">${value}</span>
+    </div>
+`
+
+// URL: http://localhost/page-1
+// SRC: img/books/default-book.png
+// Image: http://localhost/page-1/img/books/default-book.png
+
+// SRC: /img/books/default-book.png
+// Image: http://localhost/img/books/default-book.png
+
+// const a = undefined ?? 'abc'
+// // abc
+//
+// const b = 'a' ?? 'abc';
+// // a
+
+// if ( a && b ) ---
+
+const a = somevar ? 'c' : 'd';
+
+const renderBookFigure = (bookParam) => `
+    <figure class="fig-book">
+        <img src="/img/books/${bookParam.image ?? 'default-book.png'}" alt="Book Image"/>
+        <figcaption>
+            ${renderLabelValue('Name', bookParam.name ?? 'Unknown Book')}
+            ${renderLabelValue('Author', bookParam.author ?? 'Unknown Author')}
+            ${bookParam.description ? renderLabelValue('Description', bookParam.description) : ''}
+        </figcaption>
+    </figure>
+`;
 
 function renderTable(bookParam) {
     if (bookParam.length === 0) {
@@ -55,4 +92,10 @@ function renderTable(bookParam) {
     return `<table class="table-book"><thead>${htmlThead}</thead><tbody>${htmlBody}</tbody></table>`;
 }
 
-document.body.innerHTML = `<div class="d-inline mr-2">${renderTable(booksAction)}</div><div class="d-inline">${renderTable(booksAdventure)}</div>`;
+document.body.innerHTML = `
+    ${booksAction.map(book => `
+        <div class="d-inline mr-2">
+            ${renderBookFigure(book)}
+        </div>
+    `).join('')}
+`;
